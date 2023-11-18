dbfa42b6d82a78e475611459248d33d2b1f4095b
Nic Luciano <nic.luciano@gmail.com>
2023-11-18 14:35:26 +0000

    it works :o

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,49 +3,14 @@ import { Effect, pipe } from "effect";
 import * as DB from "./DB";
 import * as Runtime from "./Runtime";
 
-// const prisma: PrismaClient = new PrismaClient();
-
-// console.log(prisma);
-
-// console.log("foo is", prisma);
-
-// const foo = async () => {
-//   const newUser = await prisma.user.create({
-//     data: {
-//       username: "xxx",
-//       password: "word",
-//       email: "alice@prisma.io",
-//     },
-//   });
-
-//   console.log("newUser is ", newUser);
-// };
-
-// foo();
+const print = pipe(Effect.log("Hello"), Effect.withSpan("print"));
 
 const main = pipe(
   DB.resource,
-  Effect.flatMap((db) =>
-    pipe(
-      DB.query(db.user.findMany)(),
-      Effect.flatMap((users) =>
-        pipe(
-          Effect.log("Hello"),
-          Effect.withSpan("d"),
-          Effect.withSpan("b"),
-          Effect.withSpan("rrrrrr"),
-          Effect.repeatN(5),
-          Effect.annotateSpans("working", true),
-        ),
-      ),
-    ),
-  ),
+  Effect.flatMap((db) => DB.query(db.user.findMany)()),
+  Effect.flatMap((users) => pipe(print, Effect.repeatN(5))),
   Effect.scoped,
+  Effect.withSpan("main"),
 );
-
-/*
-TODO:
-make a resource, for a service, for the db.
-*/
 
 Runtime.run(main);
