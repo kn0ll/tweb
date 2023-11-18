b60113469707bd0eb244d85daaa6a39f368632d1
Nic Luciano <nic.luciano@gmail.com>
2023-11-18 18:54:44 +0000

    idk

--- packages/api/src/bin/serve.ts
+++ packages/api/src/bin/serve.ts
@@ -1,26 +1,12 @@
-import * as Http from "@effect/platform-node/HttpServer";
 import { runMain } from "@effect/platform-node/Runtime";
-import * as Server from "@effect/rpc-http/Server";
 import { Console, Effect, pipe } from "effect";
-import { createServer } from "http";
 
-import * as Router from "../Router.js";
-
-const server = pipe(
-  Http.router.empty,
-  Http.router.post("/rpc", Server.make(Router.make)),
-  Http.server.serve(Http.middleware.logger),
-  Effect.scoped,
-);
+import * as Server from "../Server.js";
 
 pipe(
   Console.log("Listening on http://localhost:3000"),
-  Effect.zipRight(server),
-  Effect.provide(
-    Http.server.layer(createServer, {
-      port: 3000,
-    }),
-  ),
+  Effect.zipRight(Server.make),
+  Effect.provide(Server.layer),
   Effect.tapErrorCause(Effect.logError),
   runMain,
 );
