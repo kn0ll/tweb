9c3a4c0d6fd37c6bc851deef129f0e62c7e89525
Nic Luciano <nic.luciano@gmail.com>
2023-11-20 20:52:38 +0000

    types

--- src/HTTP.tsx
+++ src/HTTP.tsx
@@ -1,10 +1,11 @@
-import { Method } from "@effect/platform/Http/Method";
+import type { Method } from "@effect/platform/Http/Method";
+import type { ReadonlyRecord } from "effect";
 
 export type Path = `/${string}`;
 
 export type Hash = `#${string}`;
 
-export type Search = Record<string, string | string[]>;
+export type Search = ReadonlyRecord.ReadonlyRecord<string | string[]>;
 
 export type Location = {
   method: Method;
