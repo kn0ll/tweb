94ae9cc30eedd326b9fcc1ec035f795a21c26813
Nic Luciano <nic.luciano@gmail.com>
2023-11-21 17:57:52 +0000

    org

--- packages/core/src/Route.tsx
+++ packages/core/src/Route.tsx
@@ -2,9 +2,7 @@ import type { Schema } from "@effect/schema";
 import type { Location } from "./HTTP.js";
 
 import * as ServerResponse from "@effect/platform/Http/ServerResponse";
-import { Effect, flow } from "effect";
-
-import * as Response from "./Response.js";
+import { Effect } from "effect";
 
 export type Route<R, E, A extends Location> = readonly [
   schema: Schema.Schema<A>,
@@ -13,8 +11,3 @@ export type Route<R, E, A extends Location> = readonly [
 
 export const make = <R, E, A extends Location>(...route: Route<R, E, A>) =>
   route;
-
-export const page = <A extends Location>(
-  schema: Schema.Schema<A>,
-  component: (_a: A) => JSX.Element,
-) => make(schema, flow(component, Response.react, Effect.succeed));
