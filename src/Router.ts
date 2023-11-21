2e53146231278f484a620b2e9117d03cffd9f4d7
Nic Luciano <nic.luciano@gmail.com>
2023-11-21 03:37:10 +0000

    ignoring app but it works

---
+++ src/Router.ts
@@ -0,0 +1,74 @@
+import * as ServerRequest from "@effect/platform/Http/ServerRequest";
+import * as ServerResponse from "@effect/platform/Http/ServerResponse";
+import { Schema } from "@effect/schema";
+import {
+  Effect,
+  flow,
+  identity,
+  Match,
+  pipe,
+  ReadonlyArray,
+  ReadonlyRecord,
+} from "effect";
+import querystring from "node:querystring";
+
+type Route<R, E, A> = [
+  schema: Schema.Schema<A>,
+  handler: (_a: A) => Effect.Effect<R, E, ServerResponse.ServerResponse>,
+];
+// Router.searchParams
+const parsePath = (path: string) =>
+  Effect.sync(() => {
+    const [pathname, search] = path.split("?");
+
+    return {
+      pathname: pathname || "",
+      search: search ? querystring.parse(search) : null,
+    };
+  });
+
+const parseRequest = ({ url, method }: ServerRequest.ServerRequest) =>
+  pipe(parsePath(url), Effect.map(ReadonlyRecord.union({ method }, identity)));
+
+export const make = (router = []) => router;
+
+export const add = <R, E, A>(route: Route<R, E, A>) =>
+  ReadonlyArray.append(route);
+
+export const app = <R, E>(_routes: Route<R, E, any>[]) =>
+  pipe(
+    ServerRequest.ServerRequest,
+    Effect.flatMap(parseRequest),
+    Effect.flatMap(
+      flow(
+        Match.value,
+        // Match.when(Schema.is(routes[0]![0]), (a) => routes[0]![1](a as A)),
+        // ...x,
+        Match.orElse(() =>
+          Effect.succeed(ServerResponse.empty({ status: 404 })),
+        ),
+      ),
+    ),
+  );
+
+// const foo = (routes: Route<unknown, unknown, unknown>[]) => (input: unknown) =>
+// Effect.iterate(
+//   { routes, result: Option.none<ServerResponse.ServerResponse>() },
+//   {
+//     while: ({ result }) => Option.isNone(result),
+//     body: ({ routes, result }) =>
+//       pipe(
+//         routes,
+//         ReadonlyArray.head,
+//         Effect.flatMap(([schema, handler]) =>
+//           pipe(
+//             input,
+//             Schema.parse(schema),
+//             (x) => x,
+//             () => Effect.succeed({ routes, result }),
+//           ),
+//         ),
+//         () => Effect.succeed({ routes, result }),
+//       ),
+//   },
+// );
--- src/server.tsx
+++ src/server.tsx
@@ -1,3 +1,4 @@
+// import * as ServerResponse from "@effect/platform/Http/Router";
 import * as ServerResponse from "@effect/platform/Http/ServerResponse";
 import * as Http from "@effect/platform-node/HttpServer";
 import * as Runtime from "@effect/platform-node/Runtime";
@@ -5,34 +6,95 @@ import { Schema } from "@effect/schema";
 import { Effect, pipe } from "effect";
 import { createServer } from "node:http";
 
-import { router } from "./router";
+import * as Router from "./Router";
+
+// TODO: check if Router.fromIterable allows for multiple R/E?
+// Router.fromIterable;
+
+// To fix, this migiht work?
+/*
+
+const add = <OR, OE, OA>(router: Router<OR, OE, OA>) => <R, E, A>(route: [
+  schema: Schema.Schema<A>,
+  handler: (_a: A) => Effect.Effect<R, E, ServerResponse.ServerResponse>,
+]) => Router<OR | R, OE | E, OA | A>
 
 pipe(
-  router([
-    [
-      Schema.struct({
-        method: Schema.literal("GET"),
-        pathname: Schema.literal("/"),
-        search: Schema.null,
-      }),
-      () => pipe("Hello World", ServerResponse.text, Effect.succeed),
-    ],
-    // [
-    //   Schema.struct({
-    //     method: Schema.literal("GET"),
-    //     pathname: Schema.literal("/x"),
-    //     // hash: Schema.null,
-    //     search: Schema.null,
-    //     // hash: Schema.union(Schema.null, Schema.literal("#description")),
-    //     // search: Schema.struct({ username: Schema.string }),
-    //   }),
-    //   () => pipe("Hello World", ServerResponse.text, Effect.succeed),
-    // ],
+  Router.empty,
+  Router.add(schema, handler),
+  Router.add(schema, handler),
+  Router.add(schema, handler),
+)
+*/
+
+pipe(
+  Router.make(),
+  Router.add([
+    Schema.struct({
+      method: Schema.literal("GET"),
+      pathname: Schema.literal("/"),
+      search: Schema.null,
+    }),
+    () => pipe("Home Page", ServerResponse.text, Effect.succeed),
   ]),
+  Router.add([
+    Schema.struct({
+      method: Schema.literal("GET"),
+      pathname: Schema.literal("/sign-up"),
+      search: Schema.null,
+    }),
+    () => pipe("Sign Up", ServerResponse.text, Effect.succeed),
+  ]),
+  Router.app,
   (x) => x,
   Http.server.serve(),
+  (x) => x,
   Effect.scoped,
   Effect.provide(Http.server.layer(createServer, { port: 8080 })),
   Effect.catchAllCause(Effect.logError),
   Runtime.runMain,
 );
+
+// pipe(
+//   Router.make(),
+//   (x) => x,
+//   Router.add([
+//     Schema.struct({
+//       method: Schema.literal("GET"),
+//       pathname: Schema.literal("/"),
+//       search: Schema.null,
+//     }),
+//     () => pipe("Hello World", ServerResponse.text, Effect.succeed),
+//   ]),
+//   (x) => x,
+// );
+
+// pipe(
+//   // router([
+//   //   [
+//   //     Schema.struct({
+//   //       method: Schema.literal("GET"),
+//   //       pathname: Schema.literal("/"),
+//   //       search: Schema.null,
+//   //     }),
+//   //     () => pipe("Hello World", ServerResponse.text, Effect.succeed),
+//   //   ],
+//   //   // [
+//   //   //   Schema.struct({
+//   //   //     method: Schema.literal("GET"),
+//   //   //     pathname: Schema.literal("/x"),
+//   //   //     // hash: Schema.null,
+//   //   //     search: Schema.null,
+//   //   //     // hash: Schema.union(Schema.null, Schema.literal("#description")),
+//   //   //     // search: Schema.struct({ username: Schema.string }),
+//   //   //   }),
+//   //   //   () => pipe("Hello World", ServerResponse.text, Effect.succeed),
+//   //   // ],
+//   // ]),
+//   // (x) => x,
+//   Http.server.serve(),
+//   Effect.scoped,
+//   Effect.provide(Http.server.layer(createServer, { port: 8080 })),
+//   Effect.catchAllCause(Effect.logError),
+//   Runtime.runMain,
+// );
