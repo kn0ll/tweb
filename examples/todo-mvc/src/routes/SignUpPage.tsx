e8e23b3e13799f531f2dba372b0009a94e3a10f0
Nic Luciano <nic.luciano@gmail.com>
2023-11-21 17:57:41 +0000

    org

--- examples/todo-mvc/src/routes/SignUpPage.tsx
+++ examples/todo-mvc/src/routes/SignUpPage.tsx
@@ -1,6 +1,7 @@
 import { Schema } from "@effect/schema";
+import { Effect, flow } from "effect";
 import * as React from "react";
-import { Link, Route } from "tweb";
+import { Link, Response, Route } from "tweb";
 
 import { Layout } from "../Layout.js";
 import { SignUpForm } from "./SignUpForm.js";
@@ -14,24 +15,31 @@ export const signUpPageSchema = Schema.struct({
 
 export const SignUpLink = Link.make(signUpPageSchema);
 
-export default Route.page(signUpPageSchema, () => (
-  <Layout>
-    <h1>Sign Up</h1>
-    {/* 1. probably need to accept same props as link. ie method="GET" (we can use this to determine what Input provides, query or body)
-    2. on that notes, query will need to be set on `action` by us... */}
-    <SignUpForm method="POST" pathname="/sign-up" search={null}>
-      {(Input) => (
-        <>
-          <label htmlFor="username">Username</label>
-          <Input type="text" name="username" id="username" />
-          <label htmlFor="email">Email</label>
-          <Input type="text" name="email" id="email" />
-          <label htmlFor="password">Password</label>
-          <Input type="password" name="password" id="password" />
-          <input type="submit" />
-          {/* <Input type="submit" />  */}
-        </>
-      )}
-    </SignUpForm>
-  </Layout>
-));
+export default Route.make(
+  signUpPageSchema,
+  flow(
+    () => (
+      <Layout>
+        <h1>Sign Up</h1>
+        {/* 1. probably need to accept same props as link. ie method="GET" (we can use this to determine what Input provides, query or body)
+            2. on that notes, query will need to be set on `action` by us... */}
+        <SignUpForm method="POST" pathname="/sign-up" search={null}>
+          {(Input) => (
+            <>
+              <label htmlFor="username">Username</label>
+              <Input type="text" name="username" id="username" />
+              <label htmlFor="email">Email</label>
+              <Input type="text" name="email" id="email" />
+              <label htmlFor="password">Password</label>
+              <Input type="password" name="password" id="password" />
+              <input type="submit" />
+              {/* <Input type="submit" />  */}
+            </>
+          )}
+        </SignUpForm>
+      </Layout>
+    ),
+    Response.react,
+    Effect.succeed,
+  ),
+);
