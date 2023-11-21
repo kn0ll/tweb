865d9802018c6b11f99173b7542c5b0ea35a779b
Nic Luciano <nic.luciano@gmail.com>
2023-11-21 18:29:31 +0000

    org

--- examples/todo-mvc/bin/server.ts
+++ examples/todo-mvc/bin/server.ts
@@ -2,9 +2,9 @@ import * as Runtime from "@effect/platform-node/Runtime";
 import { pipe } from "effect";
 import { Server } from "tweb";
 
-import { homePageRoute } from "../src/HomePage.js";
-import { signUpFormRoute } from "../src/SignUpForm.js";
-import { signUpPageRoute } from "../src/SignUpPage.js";
+import { homePageRoute } from "../src/routes/HomePage.js";
+import { signUpFormRoute } from "../src/routes/SignUpForm.js";
+import { signUpPageRoute } from "../src/routes/SignUpPage.js";
 
 pipe(
   Server.make([homePageRoute, signUpFormRoute, signUpPageRoute]),
